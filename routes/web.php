6f5e171210d2bb42bbd5a2117a712ea4d317d538
vagrant <vagrant@homestead>
2019-06-06 17:09:38 +0800

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,22 @@
 Route::get('/', 'StaticPagesController@home');
 Route::get('/help', 'StaticPagesController@help');
 Route::get('/about', 'StaticPagesController@about');
+
+Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
+
+//等同于
+//显示所有用户列表的页面
+//Route::get('/users', 'UsersController@index')->name('users.index');
+//创建用户的页面
+//Route::get('/users/create', 'UsersController@create')->name('users.create');
+//显示用户个人信息的页面
+//Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+//创建用户
+//Route::post('/users', 'UsersController@store')->name('users.store');
+//编辑用户个人资料的页面
+//Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+//更新用户
+//Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+//删除用户
+//Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
